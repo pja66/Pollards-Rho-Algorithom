a64fbe82415d32d6359cad6b0ae1f9771eb39baf
Philip Ayoub <63018034+pja66@users.noreply.github.com>
2020-04-01 16:28:00 -0400

    Update factor.java

--- factor.java
+++ factor.java
@@ -1,33 +1,59 @@
 
 public class Factor {
 
-
-	public static void main(String[] args) {
-		int n = 1037;
-		int start = 44;
-		int i =1;
-
-
-		int x = func(n,i, start);
-		int y = func(n,2*i, start);
-
-		System.out.print(x);
-		System.out.print(y);
-	  }
-
-
-	public int func(int n, int i, int start)
+	public static void main(String[] args){
+		
+	
+		int n = 25; 
+		int start = (int)(Math.random()*n-1); 
+		int i = 1;
+		int gcd = n;
+		int x;
+		int y;
+		int value;
+		
+		
+	while(gcd == n)
 	{
-
+		start = (int)(Math.random()*n-1); 	
+		x = func(n,i, start);
+		y = func(n,i*2, start);
+		value = Math.abs(x-y);
+		gcd = GCD(n,value);
+		
+		
+		while(gcd ==1) 
+		{
+			i = i +1;
+			x = func(n,i, start);
+			y = func(n,i*2, start);
+			value = Math.abs(x-y);
+			gcd = GCD(n,value);
+		}
+	}
+		
+	System.out.println("This is a factor of " + n +": "+ gcd);
+}
+	
+	
+	public static int func(int n, int i, int start)
+	{
 		if(i-1 == 0)
 		{
-			return (int)Math.pow(start, 2) - 1;
+			return ((int)Math.pow(start, 2) - 1)%n;
 		}
 		else
 		{
-			return (int)Math.pow(func(n,i-1, start), 2) - 1;
+			return ((int)Math.pow(func(n,i-1, start), 2) - 1)%n; 
 		}
 
 	}
-
+	
+	public static int GCD(int n, int value)
+	{
+		if(value == 0)
+			return n;
+		else
+			return GCD(value, n%value);
+	} 
 }
